12191d3cd4f3343ba514df8e373da1ad0febdffb
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2025-04-10 21:27:01 +0200

    init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Proteus
+Proteus (named after the Greek shape-shifting sea god) is a modern, bidirectional type adapting library for Java.
---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("java-library")
+}
+
+group = "io.github.kaktushose"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
