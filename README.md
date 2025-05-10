1e2f4108e772abd17b0ef8a819b343b716bb87ec
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2025-05-10 16:07:17 +0200

    remove files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Proteus
-Proteus (named after the Greek shape-shifting sea god) is a modern, bidirectional type adapting library for Java.
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module io.github.kaktushose.proteus {
-    requires org.jetbrains.annotations;
-
-    exports io.github.kaktushose.proteus.mapping;
-    exports io.github.kaktushose.proteus.conversion;
-    exports io.github.kaktushose.proteus.type;
-    exports io.github.kaktushose.proteus;
-}
