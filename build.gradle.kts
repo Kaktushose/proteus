2743dd7052d004d8cdcb9f5db18aef7883b22ccf
Kaktushose <42280757+Kaktushose@users.noreply.github.com>
2025-05-10 17:27:39 +0200

    Release 0.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+import org.jreleaser.model.Active
+
 plugins {
     id("java-library")
+    id("maven-publish")
+    id("org.jreleaser") version "1.18.0"
 }
 
 group = "io.github.kaktushose"
-version = "1.0.0"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
@@ -16,6 +20,88 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set("Proteus")
+                description.set("A modern, bidirectional type adapting library for Java.")
+                url.set("https://github.com/Kaktushose/proteus")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+
+                developers {
+                    developer {
+                        name.set("Kaktushose")
+                    }
+                    developer {
+                        name.set("Goldmensch")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/kaktushose/proteus.git")
+                    developerConnection.set("scm:git:ssh://github.com/kaktushose/proteus.git")
+                    url.set("https://github.com/Kaktushose/proteus")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            setUrl(layout.buildDirectory.dir("staging-deploy"))
+        }
+    }
+}
+
+jreleaser {
+    project {
+        copyright = "Kaktushose & Goldmensch"
+    }
+
+
+    signing {
+        active = Active.ALWAYS
+        armored = true
+    }
+
+    deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    active = Active.ALWAYS
+                    url = "https://central.sonatype.com/api/v1/publisher"
+                    stagingRepository("build/staging-deploy")
+                    setStage("UPLOAD")
+                }
+            }
+        }
+    }
+}
+
+tasks.jreleaserDeploy {
+    dependsOn(tasks.publish)
+}
+
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.javadoc {
+    val options = options as StandardJavadocDocletOptions
+    options.overview = "src/main/javadoc/overview.md"
+    options.links = listOf(
+        "https://google.github.io/guice/api-docs/7.0.0/javadoc/"
+    )
+    options.encoding = "UTF-8"
+    options.addBooleanOption("Xdoclint:none,-missing", true)
+    options.tags("apiNote:a:API Note:", "implSpec:a:Implementation Requirements:", "implNote:a:Implementation Note:")
 }
